40200e7707ed4fe179311607a2fcbf17c90c2aaa
Kevin Valentine <kevinva@sina.com>
2012-05-15 19:36:32 +0800

    project create!

---
+++ src/cn/kevin/seabedwallpaper/MySeabedWallpaperActivity.java
@@ -0,0 +1,13 @@
+package cn.kevin.seabedwallpaper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MySeabedWallpaperActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
