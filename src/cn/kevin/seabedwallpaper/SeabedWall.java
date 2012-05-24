3e7a41d2ae849b0fe7d2e73a8b729a6be31e26d7
Kevin Valentine <archerkevin@yeah.net>
2012-05-24 22:54:45 +0800

    !!!

--- src/cn/kevin/seabedwallpaper/SeabedWall.java
+++ src/cn/kevin/seabedwallpaper/SeabedWall.java
@@ -1,12 +1,21 @@
 package cn.kevin.seabedwallpaper;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.ComposeShader;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 
 public class SeabedWall extends WallpaperService {
@@ -19,7 +28,7 @@ public class SeabedWall extends WallpaperService {
 		return new SeabedEngine();
 	}
 	
-	public class SeabedEngine extends Engine{
+	public class SeabedEngine extends Engine implements OnSharedPreferenceChangeListener{
 		
 		private Handler mHandler = new Handler(){
 			
@@ -33,21 +42,51 @@ public class SeabedWall extends WallpaperService {
 			
 		};
 		
+		public int[] RED = new int[]{80, 255, 93, 97};
+		public int[] BLUE = new int[]{100, 2, 0, 180};
+		public int[] GREEN = new int[]{80, 62, 255, 69};
+		
+		private String bgColorStr = null;
+		private String bgTypeStr = null;
+		
 		private int screenHeight = -1;
 		private int screenWidth = -1;		
 		private float sizeRate = 1.0f;
 		private float xPos1 = 0.0f;
 		private float xPos2 = 0.0f;
+		private float touchPointX = -1.0f;
+		private float touchPointY = -1.0f;
+		private boolean touching = false;
 		private Bitmap background = null;
 		private Bitmap wave1 = null;
 		private Bitmap wave2 = null;
-		private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		private Bitmap touchBitmap = null;
+		private Paint bgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		private Paint wavePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		private BitmapShader bgBitmapShader;
+		private LinearGradient bgLinearGradient;
+		private ComposeShader bgComposeShader;
 		
 		@Override
 		public void onCreate(SurfaceHolder surfaceHolder) {
 			// TODO Auto-generated method stub
 			super.onCreate(surfaceHolder);
 			setTouchEventsEnabled(true);
+
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(SeabedWall.this);
+			prefs.registerOnSharedPreferenceChangeListener(this);
+			this.bgColorStr = prefs.getString("background_color", "None");			
+			this.bgTypeStr = prefs.getString("background_type", "Texture");
+			
+			String touchStr = prefs.getString("touch_icon", "None");
+			if(touchStr.equals("Circle")){
+				Bitmap tempBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.touch_circle);
+				this.touchBitmap = Bitmap.createScaledBitmap(tempBitmap, tempBitmap.getWidth() / 2, tempBitmap.getHeight() / 2, true);				
+			}else if(touchStr.equals("Heart")){
+				Bitmap tempBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.touch_heart);
+				this.touchBitmap = Bitmap.createScaledBitmap(tempBitmap, tempBitmap.getWidth() / 2, tempBitmap.getHeight() / 2, true);
+			}
+			
 		}
 
 		@Override
@@ -55,14 +94,14 @@ public class SeabedWall extends WallpaperService {
 			// TODO Auto-generated method stub
 			super.onDestroy();
 			mHandler.removeMessages(DRAW);
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(SeabedWall.this);
+			prefs.unregisterOnSharedPreferenceChangeListener(this);
 		}
 
 		@Override
 		public void onSurfaceCreated(SurfaceHolder holder) {
 			// TODO Auto-generated method stub
-			super.onSurfaceCreated(holder);
-
-			
+			super.onSurfaceCreated(holder);			
 			mHandler.sendEmptyMessage(DRAW);
 		}
 
@@ -71,6 +110,10 @@ public class SeabedWall extends WallpaperService {
 			// TODO Auto-generated method stub
 			super.onSurfaceDestroyed(holder);
 			mHandler.removeMessages(DRAW);
+			if(this.background != null){
+				this.background.recycle();
+				this.background = null;
+			}
 		}		
 		
 		private void drawWallpaper(){
@@ -83,7 +126,12 @@ public class SeabedWall extends WallpaperService {
 				this.screenHeight = canvas.getHeight();
 			}
 			drawBackground(canvas);
-			drawWave(canvas);
+			drawWave(canvas);	
+			if(touching && touchBitmap != null){
+				System.out.println("draw touch!!!");
+				
+				canvas.drawBitmap(touchBitmap, touchPointX, touchPointY, bgPaint);
+			}
 			holder.unlockCanvasAndPost(canvas);
 			
 			mHandler.removeMessages(DRAW);
@@ -91,14 +139,38 @@ public class SeabedWall extends WallpaperService {
 		}
 		
 		private void drawBackground(Canvas canvas){
-			if(this.background == null){
-				Bitmap newBackground = BitmapFactory.decodeResource(getResources(), R.drawable.background);
-				this.sizeRate = this.screenWidth * 1.0f / newBackground.getWidth();
-				this.background = Bitmap.createScaledBitmap(newBackground, screenWidth, screenHeight, true);
-			}				
 			
-			canvas.drawBitmap(background, 0, 0, this.mPaint);
-			canvas.drawARGB(128, 90, 100, 110);
+			if(this.background == null){
+				if(this.bgTypeStr.equals("Texture")){					
+					Bitmap newBackground = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+					this.sizeRate = this.screenWidth * 1.0f / newBackground.getWidth();
+					this.background = Bitmap.createScaledBitmap(newBackground, screenWidth, screenHeight, true);
+							
+				}else if(this.bgTypeStr.equals("Gradient")){	
+					Bitmap newBackground = BitmapFactory.decodeResource(getResources(), R.drawable.solidbackground);
+					this.sizeRate = this.screenWidth * 1.0f / newBackground.getWidth();
+					this.background = Bitmap.createScaledBitmap(newBackground, screenWidth, screenHeight, true);
+		
+				}else if(this.bgTypeStr.equals("None")){
+					
+				}
+				
+				/*
+				int[] colors = new int[]{Color.WHITE, Color.GREEN};
+				this.bgBitmapShader = null;
+				this.bgBitmapShader = new BitmapShader(this.background, Shader.TileMode.CLAMP, Shader.TileMode.MIRROR);
+				this.bgLinearGradient = null;
+				this.bgLinearGradient = new LinearGradient(0, 0, screenWidth, screenHeight, colors, null, TileMode.REPEAT);
+				this.bgComposeShader = null;
+				this.bgComposeShader = new ComposeShader(this.bgBitmapShader, this.bgLinearGradient, PorterDuff.Mode.DARKEN);
+				this.bgPaint.setShader(bgComposeShader);
+				*/
+			}
+			
+			//canvas.drawRect(0, 0, screenWidth, screenHeight, bgPaint);		
+			
+			
+			canvas.drawBitmap(background, 0, 0, bgPaint);
 		}
 		
 		private void drawWave(Canvas canvas){
@@ -125,13 +197,113 @@ public class SeabedWall extends WallpaperService {
 			}
 			if(xPos2 <= -this.wave2.getWidth()){
 				xPos2 = this.wave1.getWidth();
-			}			
-			canvas.drawBitmap(wave1, xPos1, screenHeight * 0.5f, this.mPaint);
+			}
+			
+			canvas.drawBitmap(wave1, xPos1, screenHeight * 0.5f, this.wavePaint);			
 			xPos1 -= 8;
-			canvas.drawBitmap(wave2, xPos2, screenHeight * 0.5f, this.mPaint);
+			canvas.drawBitmap(wave2, xPos2, screenHeight * 0.5f, this.wavePaint);
 			xPos2 -= 8;
 
 		}
+		
+		/*
+		private Bitmap createRGBImage(Bitmap bitmap, int color){
+			int bitmapW = bitmap.getWidth();
+			int bitmapH = bitmap.getHeight();
+			int[] arrayColor = new int[bitmapW * bitmapH];
+			int count = 0;
+			for(int i = 0; i < bitmapH; i++){
+				for(int j = 0; j < bitmapW; j++){
+					int color1 = bitmap.getPixel(j, i);
+					System.out.println("color: " + color1);
+					if(color1 != 0){
+						
+					}else{
+						color1 = color;
+					}
+					
+					arrayColor[count] = color;
+					count++;
+				}
+				
+			}
+			
+			bitmap = Bitmap.createBitmap(arrayColor, bitmapW, bitmapH, Config.ARGB_4444);
+			return bitmap;
+		}
+		*/
+
+		public void onSharedPreferenceChanged(
+				SharedPreferences sharedPreferences, String key) {
+			// TODO Auto-generated method stub
+			if(key.equals("background_color")){
+				this.bgColorStr = sharedPreferences.getString(key, "None");
+				
+			}else if(key.equals("background_type")){
+				this.bgTypeStr = sharedPreferences.getString(key, "Texture");
+				
+				if(this.background != null){
+					this.background.recycle();
+					this.background = null;
+				}
+				
+			}else if(key.equals("touch_icon")){
+				String touchStr =  sharedPreferences.getString(key, "None");
+				if(touchStr.equals("Circle")){
+					if(this.touchBitmap != null){
+						this.touchBitmap.recycle();
+						this.touchBitmap = null;						
+					}
+					Bitmap tempBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.touch_circle);
+					this.touchBitmap = Bitmap.createScaledBitmap(tempBitmap, tempBitmap.getWidth() / 2, tempBitmap.getHeight() / 2, true);
+					
+				}else if(touchStr.equals("Heart")){
+					if(this.touchBitmap != null){
+						this.touchBitmap.recycle();
+						this.touchBitmap = null;						
+					}
+					Bitmap tempBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.touch_heart);
+					this.touchBitmap = Bitmap.createScaledBitmap(tempBitmap, tempBitmap.getWidth() / 2, tempBitmap.getHeight() / 2, true);
+				}else if(touchStr.equals("None")){
+					if(this.touchBitmap != null){
+						this.touchBitmap.recycle();
+						this.touchBitmap = null;
+					}
+				}
+			}			
+		}		
+		
+
+		@Override
+		public void onTouchEvent(MotionEvent event) {
+			// TODO Auto-generated method stub
+			super.onTouchEvent(event);
+			
+			if(event.getAction() == MotionEvent.ACTION_DOWN){
+				System.out.println("down");
+				
+				touching = true;
+				this.touchPointX = event.getX() - 20;
+				this.touchPointY = event.getY() - 20;
+			}
+			
+			if(event.getAction() == MotionEvent.ACTION_MOVE){
+				System.out.println("move");
+				
+				this.touchPointX = event.getX() - 20;
+				this.touchPointY = event.getY() - 20;
+				
+				System.out.println("x=" + this.touchPointX + ", y=" + this.touchPointY);
+			}
+			
+			if(event.getAction() == MotionEvent.ACTION_UP){
+				System.out.println("up");
+				
+				touching = false;
+			}
+		}
+
+	
 		
 	}
 
--- src/cn/kevin/seabedwallpaper/SeabedWallSettingsActivity.java
+++ src/cn/kevin/seabedwallpaper/SeabedWallSettingsActivity.java
@@ -2,14 +2,27 @@ package cn.kevin.seabedwallpaper;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class SeabedWallSettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener{
+public class SeabedWallSettingsActivity extends PreferenceActivity{
 
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
 		
+		this.addPreferencesFromResource(R.xml.settings);
+		this.setContentView(R.layout.preference_main);
 	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
 
 }
